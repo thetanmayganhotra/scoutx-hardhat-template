47b057331bf7a805c2a54b7970ca06b3bcf02d15
tanmay ganhotra <tanmay.ganhotra@gmail.com>
2022-02-28 14:14:01 +0530

    Hardhat-template'v1'

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomiclabs/hardhat-waffle");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+//  const INFURA_API_KEY = "";
+//  const MUMBAI_PRIVATE_KEY = "";
+ 
+ module.exports = {
+   solidity: {
+     version: "0.8.5"
+//  },
+//   //  networks:{
+//   //    mumbai: {
+//   //      url:`https://polygon-mumbai.infura.io/v3/${INFURA_API_KEY}`,
+//   //      accounts: [`${MUMBAI_PRIVATE_KEY}`]
+//   //    }
+ 
+   }
+ }
